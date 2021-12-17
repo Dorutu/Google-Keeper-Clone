11a1e6f7516693936b62503cb61a88e9e6184f99
Dorutu <dorubota123@gmail.com>
2021-12-17 05:00:10 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nota from "@material-ui/icons/Note";
+
+function Header() {
+  return (
+    <header>
+      <h1><Nota></Nota>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
