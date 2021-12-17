821e0d4f5f7ac40340adee527fee8a517662cc63
Dorutu <dorubota123@gmail.com>
2021-12-17 13:43:31 +0200

    Updates

---
+++ precache-manifest.1095245a413dd9a6dfdfe5ec0822f677.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6f7405d39bdacdd7923e61581700ab",
+    "url": "/Google-Keeper-Clone/index.html"
+  },
+  {
+    "revision": "0983b76b90eab473cd40",
+    "url": "/Google-Keeper-Clone/static/js/2.d7254e14.chunk.js"
+  },
+  {
+    "revision": "4e2c14a401b70898e7b4",
+    "url": "/Google-Keeper-Clone/static/js/main.27df4371.chunk.js"
+  },
+  {
+    "revision": "66165f59385d01e91621",
+    "url": "/Google-Keeper-Clone/static/js/runtime-main.64da85c8.js"
+  }
+]);
